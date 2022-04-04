0987f705d18dd3195387c6c20eb05a56ff0ed6d2
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-04-04 12:35:02 +0200

    :radio: Add Canvas to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next';
+import { Canvas } from '@react-three/fiber';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Canvas>
+    <Component {...pageProps} />
+  </Canvas>
 }
 
 export default appWithTranslation(MyApp)
