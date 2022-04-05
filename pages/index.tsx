5b5b401019d71f7da73eab50431ca618f6e2dcbc
Halvor Vivelstad <halvorviv@gmail.com>
2022-04-05 20:49:57 +0200

    :ab: Add iphone model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
+import { Canvas } from "@react-three/fiber";
 import { motion } from "framer-motion";
-import { motion as motion3D } from "framer-motion-3d";
+import {
+  Environment,
+  ContactShadows,
+  OrbitControls,
+  AdaptiveDpr,
+  AdaptiveEvents,
+} from "@react-three/drei";
 import type { NextPage } from "next";
+import { Suspense } from "react";
 import { useEffect, useState } from "react";
 import { LoadingIntro } from "../components/intro";
+import { IphoneModel } from "../components/models/iphone";
 
 const mainVariants = {
   initial: {
@@ -38,7 +47,41 @@ const Home: NextPage = () => {
         transition={{ duration: 1.0 }}
         className="h-screen"
       >
-        
+        <Canvas
+          shadows
+          dpr={[1, 2]}
+          camera={{ position: [0, 0, 4], fov: 50 }}
+          className="min-h-screen"
+        >
+          <ambientLight intensity={0.5} />
+          <spotLight
+            intensity={0.3}
+            angle={0.3}
+            penumbra={1}
+            position={[5, 25, 20]}
+            shadow-bias={-0.0001}
+            castShadow
+          />
+          <Suspense fallback={null}>
+            <IphoneModel scale={20} position={[0, -5, 0]} 
+            
+            rotation={[0, 180, 180]}
+            />
+            <Environment preset="city" />
+            <ContactShadows
+              rotation-x={Math.PI / 2}
+              position={[0, -0.8, 0]}
+              opacity={0.25}
+              width={10}
+              height={10}
+              blur={1.5}
+              far={0.9}
+            />
+          </Suspense>
+          {/* <OrbitControls regress />
+      <AdaptiveDpr pixelated /> */}
+          <AdaptiveEvents />
+        </Canvas>
       </motion.div>
     </>
   );
