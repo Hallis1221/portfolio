d7ea74b3b0d4836c1d8cacd1351f9777a9fa323f
Hallis1221 <halvorviv@gmail.com>
2022-04-09 20:56:38 +0200

    Fix box shifting

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
       >
         <LandingComponent />
         <div className="h-screen" />
-        <div className="h-fit">
+        <div className="h-fit mx-10">
           <h1 className="text-center text-8xl font-semibold">
             {t("projects.title").toString()}
           </h1>
@@ -80,6 +80,7 @@ const Home: NextPage = () => {
               description={t("projects.mads.description")}
               image="mads.png"
               github="hallis1221/mads-core"
+              url="https://marketads.me"
               nextjs
               typescript
               recommended
@@ -134,17 +135,6 @@ function ContactFooter() {
   return (
     <footer className="footer footer-center p-10 bg-neutral-focus text-primary-content">
       <div>
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="inline-block fill-current"
-        >
-          <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-        </svg>
         <p className="font-bold">
           {t("footer.author").toString()}
           <br />
@@ -239,8 +229,8 @@ function LandingComponent() {
                   {t("landing.title").toString()}
                 </h1>
 
-                <div className="mb-5 flex text-center flex-row justify-center text-xl md:text-3xl">
-                  {t("landing.a").toString()}
+                <div className="mb-5 flex text-center flex-row h-20 justify-center text-xl md:text-3xl">
+                  <div className="pr-2"> {t("landing.a").toString()}</div>
                   <Typewriter
                     options={{
                       cursor: "",
@@ -251,34 +241,38 @@ function LandingComponent() {
                       typewriter
                         .pauseFor(600 * waittime)
                         .typeString(
-                          '<div class="px-2 text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-blue-800">typescript</div>'
-                        )
-
-                        .pauseFor(2500)
-                        .deleteAll()
-                        .typeString(
-                          '<div class="px-2 text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-purple-600">Next.js</div>'
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-blue-600 to-blue-800">typescript</div>'
                         )
                         .pauseFor(2500)
                         .deleteAll()
                         .typeString(
-                          '<div class="px-2 text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-pink-600 to-red-600">Remix</div>'
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-purple-600">Next.js</div>'
+                        )
+                        .deleteAll()
+                        .typeString(
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-purple-600">Next.js</div>'
                         )
                         .pauseFor(2500)
                         .deleteAll()
                         .typeString(
-                          '<div class="px-2 text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-600">flutter</div>'
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-pink-600 to-red-600">Remix</div>'
                         )
                         .pauseFor(2500)
                         .deleteAll()
                         .typeString(
-                          '<div class="px-2 text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-yellow-600">javascript</div>'
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-600">flutter</div>'
                         )
-
+                        .pauseFor(2500)
+                        .deleteAll()
+                        .typeString(
+                          '<div class="text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-yellow-600">javascript</div>'
+                        )
                         .start();
                     }}
                   />
-                  {t("landing.description").toString()}
+                  <div className="pl-2">
+                    {t("landing.description").toString()}
+                  </div>
                 </div>
               </div>
             </div>
@@ -304,6 +298,7 @@ export function ProjectComponent(
     javascript,
     image,
     github,
+    url,
   }: {
     isNew?: boolean;
     isOld?: boolean;
@@ -318,6 +313,7 @@ export function ProjectComponent(
     javascript?: boolean;
     image?: string;
     github?: string;
+    url?: string;
   } = {
     isNew: false,
     recommended: false,
@@ -367,8 +363,8 @@ export function ProjectComponent(
   return (
     <motion.div
       className="card w-96 my-5 bg-base-100 shadow-xl"
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.8, borderRadius: "10%" }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 1.05, borderRadius: "10%" }}
     >
       <figure>
         <Image
@@ -412,7 +408,13 @@ export function ProjectComponent(
         <p>{description}</p>
 
         <div className="card-actions justify-end mt-5">
-          {github ? <GithubLinkComponent repo={github} /> : <></>}
+          <div className="justify-start absolute left-5 ">
+            <div className="flex flex-row">
+              {github ? <GithubLinkComponent repo={github} /> : <></>}
+              {url ? <ViewLiveComponent url={url} /> : <></>}
+            </div>
+          </div>
+
           {nextjs ? <TagComponent techTag={nextTag} /> : <></>}
 
           {remix ? <TagComponent techTag={remixTag} /> : <></>}
@@ -449,15 +451,18 @@ export function GithubLinkComponent({ repo }: { repo: string }) {
   };
 
   return (
-    <button
-      className="justify-start absolute left-8"
-      onClick={() => window.open("https://github.com/" + repo, "_blank")}
+    <a
+      className="pl-3"
+      href={"https://github.com/" + repo}
+      target="_blank"
+      rel="noreferrer"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
         viewBox="0 0 24 24"
+        className=""
       >
         <motion.path
           initial="hidden"
@@ -466,7 +471,48 @@ export function GithubLinkComponent({ repo }: { repo: string }) {
           d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
         />
       </svg>
-    </button>
+    </a>
+  );
+}
+
+export function ViewLiveComponent({ url }: { url: string }) {
+  return (
+    <a className="pl-3" href={url} target="_blank" rel="noreferrer">
+      <svg
+        version="1.1"
+        id="Capa_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="24"
+        height="24"
+        fill="white"
+        viewBox="0 0 410 410"
+        xmlSpace="preserve"
+        className=""
+      >
+        <g>
+          <g>
+            <path
+              d="M349.957,60.043C311.237,21.323,259.756,0,205,0C150.243,0,98.762,21.323,60.043,60.043C21.323,98.762,0,150.243,0,205
+			c0,54.756,21.323,106.236,60.043,144.957C98.762,388.677,150.243,410,205,410c54.756,0,106.237-21.323,144.957-60.043
+			C388.677,311.236,410,259.756,410,205C410,150.243,388.677,98.762,349.957,60.043z M362.492,180.055h-79.54
+			c-1.593-41.652-7.159-80.379-16.072-110.998c-1.286-4.415-2.64-8.627-4.046-12.647
+			C314.781,76.697,353.565,123.478,362.492,180.055z M205,358.201c-4.181-6.247-10.438-19.219-16.121-42.676
+			c-5.904-24.359-9.692-54.093-11.012-85.58h54.266c-1.32,31.487-5.106,61.219-11.012,85.578
+			C215.438,338.982,209.18,351.954,205,358.201z M177.867,180.055c1.319-31.488,5.107-61.221,11.012-85.58
+			c5.684-23.457,11.94-36.429,16.121-42.676c4.18,6.247,10.438,19.219,16.121,42.677c5.904,24.36,9.691,54.091,11.012,85.579
+			H177.867z M147.165,56.409c-1.407,4.021-2.762,8.232-4.046,12.647c-8.914,30.619-14.479,69.346-16.072,110.998H47.506
+			C56.435,123.479,95.219,76.698,147.165,56.409z M47.506,229.945h79.541c1.594,41.651,7.158,80.379,16.072,110.998
+			c1.284,4.414,2.639,8.626,4.046,12.646C95.219,333.302,56.435,286.521,47.506,229.945z M262.834,353.591
+			c1.406-4.021,2.76-8.23,4.046-12.646c8.913-30.618,14.479-69.347,16.072-110.998h79.54
+			C353.565,286.521,314.781,333.303,262.834,353.591z"
+            />
+          </g>
+        </g>
+      </svg>
+    </a>
   );
 }
 type TechTag = {
