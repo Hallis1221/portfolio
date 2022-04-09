a3fa85a0e2c91d023b898815160c375805a5b215
Hallis1221 <halvorviv@gmail.com>
2022-04-09 22:24:48 +0200

    Allow images

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   i18n,
   images: {
-    domains: ['via.placeholder.com'],
+    domains: ['via.placeholder.com', 'halvor.codes', 'www.halvor.codes'],
   },
 }
 
